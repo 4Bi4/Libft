e7a2e6cd96a0a3017ed0a9e57e8209fab81a2f63
Lucas Abia fernandez <labia-fe@c2r6s2.42madrid.com>
2024-10-04 19:31:54 +0200

    fixed putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,25 +14,29 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	char	digit;
+
 	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else if (n < 0)
 	{
-		ft_putchar_fd('-', fd);
-		ft_putchar_fd('2', fd);
-		ft_putnbr_fd(147482648, fd);
-		return ;
-	}
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
+		write(fd, "-", 1);
 		n = -n;
-	}
-	if (n > 9)
-	{
-		ft_putnbr_fd((n / 10), fd);
-		ft_putnbr_fd((n % 10), fd);
+		ft_putnbr_fd(n, fd);
 	}
 	else
-		ft_putchar_fd((n + 48), fd);
+	{
+		if (n > 9)
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+		else
+		{
+			digit = n + '0';
+			write (fd, &digit, 1);
+		}
+	}
 }
 
 /*
